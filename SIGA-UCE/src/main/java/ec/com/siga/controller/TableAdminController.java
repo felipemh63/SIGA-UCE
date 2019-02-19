ca734acac304d7ca7a15ae174a7134c409bf2457
Lenin V. <leninsasos@hotmail.com>
2019-02-18 19:19:39 -0500

    vista add administrator

--- SIGA-UCE/src/main/java/ec/com/siga/controller/TableAdminController.java
+++ SIGA-UCE/src/main/java/ec/com/siga/controller/TableAdminController.java
@@ -1,15 +1,25 @@
 package ec.com.siga.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import ec.com.siga.repository.UserJpaRepository;
 
 @Controller
 public class TableAdminController {
-
+	@Autowired 
+	@Qualifier("userRepository") 
+	private UserJpaRepository userRepository;
+	
 	@GetMapping("/tableAdmin")
-	public String showForm() {
-		return "tableAdmin";
+	public ModelAndView showForm() {
+		ModelAndView mav = new ModelAndView("tableAdmin");
+		mav.addObject("contacts", userRepository.findAll());
+		return mav;
 	}
 
 }
