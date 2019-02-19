fa5ddf349bac361468df8f87c207dbb3d3a4a4a4
Lenin V. <leninsasos@hotmail.com>
2019-02-19 00:44:04 -0500

    edit add administrador, vista table admin

---
+++ SIGA-UCE/src/main/java/ec/com/siga/controller/EditAdminController.java
@@ -0,0 +1,38 @@
+package ec.com.siga.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import ec.com.siga.entity.User;
+import ec.com.siga.repository.UserJpaRepository;
+
+@Controller
+public class EditAdminController {
+	@Autowired 
+	@Qualifier("userRepository") 
+	private UserJpaRepository userRepository;
+	
+
+	@GetMapping("/editAdmin")
+	public String redirectContactForm(@RequestParam(name = "id", required = false) int id, Model model) {
+		model.addAttribute("contactModel", userRepository.findById(id));
+		return "/editAdmin";
+	}
+	
+	@PostMapping("/addcontact")
+	public String addContact(@ModelAttribute(name = "contactModel") User contactModel, Model model) {
+		
+		if (null != userRepository.save(contactModel)) {
+			model.addAttribute("result", 1);
+		} else {
+			model.addAttribute("result", 0);
+		}
+		return "/dashboardAdmin";
+	}
+}
--- SIGA-UCE/src/main/java/ec/com/siga/controller/TableAdminController.java
+++ SIGA-UCE/src/main/java/ec/com/siga/controller/TableAdminController.java
@@ -3,8 +3,8 @@ package ec.com.siga.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
-
 import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.servlet.ModelAndView;
 
 import ec.com.siga.repository.UserJpaRepository;
@@ -21,5 +21,14 @@ public class TableAdminController {
 		mav.addObject("contacts", userRepository.findAll());
 		return mav;
 	}
+	
+	
+	
+	
+
+	
+
+	
+	
 
 }
