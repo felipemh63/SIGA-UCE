8a027dd728bf1624277bbc770b5a93ef1ebbdee2
Lenin V. <leninsasos@hotmail.com>
2019-02-23 19:25:58 -0500

    seleccionadores de lo conserniente a direccion

--- SIGA-UCE/src/main/java/ec/com/siga/controller/AddAddressController.java
+++ SIGA-UCE/src/main/java/ec/com/siga/controller/AddAddressController.java
@@ -1,5 +1,7 @@
 package ec.com.siga.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -7,11 +9,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import ec.com.siga.entity.Direccion;
+import ec.com.siga.entity.Pais;
+import ec.com.siga.entity.ProvinciaEstado;
+import ec.com.siga.repository.CantonJpaRepository;
 import ec.com.siga.repository.DireccionRepository;
+import ec.com.siga.repository.PaisJpaRepository;
 import ec.com.siga.repository.ParroquiaJpaRepository;
+import ec.com.siga.repository.ProvinciaJpaRepository;
 
 @Controller
 public class AddAddressController {
@@ -19,6 +29,18 @@ public class AddAddressController {
 	@Autowired
 	@Qualifier("parroquiaRepository")
 	private ParroquiaJpaRepository parroquiaRepository;
+	
+	@Autowired
+	@Qualifier("cantonRepository")
+	private CantonJpaRepository cantonRepository;
+	
+	@Autowired
+	@Qualifier("provinciaRepository")
+	private ProvinciaJpaRepository provinciaRepository;
+	
+	@Autowired
+	@Qualifier("paisRepository")
+	private PaisJpaRepository paisRepository;
 
 	@Autowired
 	@Qualifier("direccionRepository")
@@ -27,8 +49,15 @@ public class AddAddressController {
 	@GetMapping("/addAddress")
 	public ModelAndView showForm() {
 		ModelAndView mav = new ModelAndView("addAddress");
-		mav.addObject("parroquias", parroquiaRepository.findAll());
+		mav.addObject("paices", paisRepository.findAll());
+		//mav.addObject("provincias", provinciaRepository.findAll());
 		return mav;
+	}
+	
+	@RequestMapping("/cargaCanton")
+	public @ResponseBody ArrayList<ProvinciaEstado> showProvince(@RequestParam(name = "id", required = true) String id) {
+		System.out.println(id);
+		return new ArrayList<> (provinciaRepository.findByPaisId(Integer.valueOf(id)));
 	}
 
 	@PostMapping("/addAddress")
--- SIGA-UCE/src/main/java/ec/com/siga/repository/ProvinciaJpaRepository.java
+++ SIGA-UCE/src/main/java/ec/com/siga/repository/ProvinciaJpaRepository.java
@@ -1,13 +1,17 @@
 package ec.com.siga.repository;
 
 import java.io.Serializable;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import ec.com.siga.entity.Pais;
 import ec.com.siga.entity.ProvinciaEstado;
 
 @Repository("provinciaRepository")
 public interface ProvinciaJpaRepository extends JpaRepository<ProvinciaEstado, Serializable>{
+	
+	public abstract List<ProvinciaEstado> findByPaisId(Pais paisId);
 	
 }
